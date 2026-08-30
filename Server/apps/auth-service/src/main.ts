a343da2bba55dba40e1eef78bbf5ac9b83b5b63c
Avirup Kar <avirupk57@gmail.com>
2026-08-30 19:30:24 +0530

    try to setup the main.ts file

--- Server/apps/auth-service/src/main.ts
+++ Server/apps/auth-service/src/main.ts
@@ -8,14 +8,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: false,
+  });
+
+  await app.listen(process.env.AUTH_SERVICE_PORT ?? 3001);
+  console.log('Auth-Service is running on port:3001')
 }
 
 bootstrap();
